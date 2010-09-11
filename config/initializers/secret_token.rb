6960338303ec4628249810d7b43bce37561bdc20
Adam Musial-Bright <amusial@gmx.net>
2010-09-11 13:33:50 +0200

    Showme processing and javasscript integration into a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showme::Application.config.secret_token = '435f6dce183184e2f7b8bd27da7202e3f9e13b76f8fa43112cb2f1296f1c86ce34a473d13d13459f31962ae9b863de69ac2d44a5b1849a53d12c23d6cf45ac67'
